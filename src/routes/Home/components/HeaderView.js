df3a8c6b2871b77744329c9f0aa870750f426afb
Jesse Heikkila <jesse@playven.com>
2017-06-09 20:20:19 -0400

    fix email url

--- src/routes/Home/components/HeaderView.js
+++ src/routes/Home/components/HeaderView.js
@@ -8,7 +8,7 @@ const HeaderView = () =>
 
       <div className='pvs flex-row flex-ha'>
         <img className='color-dark-blue ' alt='TossDeck' src={logo} />
-        <a href='mailto:decks@tossdeck.com?subject=TossDeck PitchDeck Review&body=Hey,
+        <a href='mailto:inbox@tossdeck.com?subject=TossDeck PitchDeck Review&body=Hey,
           here is a link to our Pitch Deck. Thanks for the help!'
           className='btn-link signup-btn flex-row flex-vc'>
           <div className='t4 font-semibold color-white pas'>
@@ -23,7 +23,7 @@ const HeaderView = () =>
             Toss your pitch <br />
             deck, receive free feedback
           </h1>
-          <a href='mailto:decks@tossdeck.com?subject=TossDeck PitchDeck Review&body=Hey,
+          <a href='mailto:inbox@tossdeck.com?subject=TossDeck PitchDeck Review&body=Hey,
             here is a link to our Pitch Deck. Thanks for the help!'
             className='flex-row flex-vc flex-hc'>
             <div className='btn t4 color-white'>
